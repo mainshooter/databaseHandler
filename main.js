6df6bf0c6c65be7f0bb8314db96057a14e3de7b0
Peter 4A5A <498883@edu.rocmn.nl>
2017-03-09 09:00:29 +0100

    AJAX JS and loader for ajax

---
+++ main.js
@@ -0,0 +1,79 @@
+function getRequest() {
+  // Request all the info for every user
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  stateShow();
+  xhttp.open("GET", "crtl.database.php?submit=table", true);
+  xhttp.send("");
+}
+function updateData(id) {
+  // Update data
+  var firstname = document.getElementById('first_name' + id).value;
+  var secondname = document.getElementById('last_name' + id).value;
+  var usercity = document.getElementById('user_city' + id).value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     showResult(this.responseText);
+     getRequest();
+     stateShow();
+    }
+  };
+  stateShow();
+  var postParameters = "submit=update&first_name=" + firstname + "&last_name=" + secondname + "&user_city=" + usercity + "&user_id=" + id;
+  // contains te post values
+  xhttp.open("POST", "crtl.database.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(postParameters);
+}
+function insertData(firstname, secondname, usercity) {
+  // Insert data into the database
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     showResult(this.responseText);
+     getRequest();
+    }
+  };
+
+  var postParameters = "submit=create&first_name=" + firstname + "&last_name=" + secondname + "&user_city=" + usercity;
+  // contains te post values
+  xhttp.open("POST", "crtl.database.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(postParameters);
+}
+function deleteData(userID) {
+  // This function delete a row
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     showResult(this.responseText);
+     getRequest();
+    }
+  };
+  xhttp.open("GET", "crtl.database.php?submit=remove&user_id=" + userID + "", true);
+  xhttp.send("");
+}
+function showResult(message) {
+  // We use this to display a message
+  document.getElementById('result').innerHTML = message;
+}
+
+var status = "loading";
+function stateShow() {
+  var stateDiv = document.getElementById('status');
+  // This function change the div status
+  // So that the user can see what is happening
+  if (status == "loading") {
+    stateDiv.className = "loader";
+    status = "clear";
+  }
+  else if (status == "clear") {
+    stateDiv.className = "checked";
+    status = "loading";
+  }
+}
