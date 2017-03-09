633d4b71e6d7982eae5862e73b09414b9f3a0a44
Peter 4A5A <498883@edu.rocmn.nl>
2017-03-09 10:07:26 +0100

    Added some bootstrap!

--- view.class.php
+++ view.class.php
@@ -16,15 +16,17 @@
       foreach ($row as $key) {
         echo "
           <tr>
+          <div class='form-group'>
             <form method='post' class='form-group' action='crtl.database.php'>
               <input type='hidden' name='user_id' id='userID' value='" . $key['user_id'] . "'>
               <td>" . $key['user_id'] . "</td>
               <td><input type='text' class='form-control' name='first_name' id='first_name" . $key['user_id'] . "' value='" . $key['first_name'] . "'></td>
               <td><input type='text' class='form-control' name='last_name' id='last_name" . $key['user_id'] . "' value='" . $key['last_name'] . "'></td>
               <td><input type='text' class='form-control' name='user_city' id='user_city" . $key['user_id'] . "' value='" . $key['user_city'] . "'></td>
-              <td><button type='button' onclick=updateData(" . $key['user_id'] . ");>Update</button></td>
-              <td><button type='button' onclick=deleteData(" . $key['user_id'] . ");>Remove!</button></td>
+              <td><button class='btn btn-secondary' type='button' onclick=updateData(" . $key['user_id'] . ");>Update</button></td>
+              <td><button class='btn btn-secondary' type='button' onclick=deleteData(" . $key['user_id'] . ");>Remove!</button></td>
             </form>
+            </div>
           </tr>
       </form>
         ";
